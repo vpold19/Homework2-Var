2ee465a09e0fd2ca5b6550293eeb90deebbd9ee7
vadim <vadimpoldolin19@gmail.com>
2023-02-13 21:02:05 +0300

    Задачи 1 - 3

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        var dog=8.0;
+        System.out.println(dog);
+        var cat=3.6;
+        System.out.println(cat);
+        var paper=763789;
+        System.out.println(paper);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        var dog=8.0;
+        System.out.println(dog);
+        dog=dog+4;
+        System.out.println(dog);
+        var cat=3.6;
+        System.out.println(cat);
+        cat=cat+4;
+        System.out.println(cat);
+        var paper=763789;
+        System.out.println(paper);
+        paper=paper+4;
+        System.out.println(paper);
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        var dog=8.0;
+        System.out.println(dog);
+        dog=dog+4;
+        System.out.println(dog);
+        dog=dog-3.5;
+        System.out.println(dog);
+        var cat=3.6;
+        System.out.println(cat);
+        cat=cat+4;
+        System.out.println(cat);
+        cat=cat-1.6;
+        System.out.println(cat);
+        var paper=763789;
+        System.out.println(paper);
+        paper=paper+4;
+        System.out.println(paper);
+        paper=paper-7639;
+        System.out.println(paper);
+    }
+}
