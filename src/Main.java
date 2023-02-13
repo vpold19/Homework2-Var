3b7d30a58160ccd18c4fcf45bd8f0e705d73cd80
vadim <vadimpoldolin19@gmail.com>
2023-02-13 23:27:30 +0300

    Задачи 4 - 5

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+        task5();
     }
 
     public static void task1() {
@@ -50,5 +52,25 @@ public class Main {
         System.out.println(paper);
         paper=paper-7639;
         System.out.println(paper);
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        var friend=19;
+        System.out.println(friend);
+        friend=friend+2;
+        System.out.println(friend);
+        friend=friend / 7;
+        System.out.println(friend);
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        var frog= 3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
     }
 }
